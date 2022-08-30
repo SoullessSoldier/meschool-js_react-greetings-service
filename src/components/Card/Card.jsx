146dcec608237676473f169fda8f64468692b14b
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2022-08-30 23:41:31 +0200

    add Day1

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,15 +2,17 @@ import CardImage from "./CardImage/CardImage";
 import CardFelicitation from "./CardFelicitation/CardFelicitation";
 import Container from "../Container/Container";
 
-import style from './Card.module.css';
+import style from "./Card.module.css";
 
 const Card = (props) => {
-    <Container>
-        <div className={style.wrapper}>
-            <CardImage/>
-            <CardFelicitation text={props.text}/>
-        </div>
-    </Container>
+  <Container>
+    <div className={style.card}>
+      <div className={style.wrapper}>
+        <CardImage />
+        <CardFelicitation text={props.text} />
+      </div>
+    </div>
+  </Container>;
 };
 
 export default Card;
